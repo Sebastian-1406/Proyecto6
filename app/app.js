00b1af26f30d350b3b9d8ba017c86fe7c95bcb55
Sebastian1406 <SCsebastian1406@gmail.com>
2021-07-17 18:30:30 -0500

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,13 @@
+let boton = document.querySelector(".fa-hamburger")
+let body = document.querySelector("body")
+
+boton.addEventListener("click", () => {
+
+    body.classList.toggle("active")
+
+    if (className = "active") {
+        
+        document.querySelector(".fa-hamburger").classList.replace("fa-hamburger", "fa-times")
+    }
+
+})
